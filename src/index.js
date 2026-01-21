71f1713494fd7ab03e4f7221692c60bcb9d74020
Xavier Miller <Zebony9@gmail.com>
2026-01-21 13:51:49 +1300

    Add menu page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import "./style.css";
 import { loadHomePage } from "./home.js"
+import { loadMenuPage } from "./menu.js"
 
 console.log("Hello, World!");
 
 loadHomePage();
+
+const homeBtn = document.querySelector("#home");
+homeBtn.addEventListener("click", () => {
+    loadHomePage();
+})
+
+const menuBtn = document.querySelector("#menu");
+menuBtn.addEventListener("click", () => {
+    loadMenuPage();
+})
+
+const aboutBtn = document.querySelector("#about");
+aboutBtn.addEventListener("click", () => {
+    loadAboutPage();
+})
--- src/menu.js
+++ src/menu.js
@@ -2,7 +2,14 @@ export function loadMenuPage() {
     const content = document.querySelector(".content");
     content.innerHTML = "";
 
-    const foodItems = document.createElement("section");
-    foodItems.className = "food";
-    
+    const food = document.createElement("section");
+    food.className = "food";
+
+    for (let i = 0; i < 8; i++) {
+        const foodItem = document.createElement("div");
+        foodItem.className = "foodItem";
+        food.appendChild(foodItem);
+    }
+
+    content.appendChild(food);
 }
