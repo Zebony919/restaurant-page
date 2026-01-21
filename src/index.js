6d54d555986fd79ec73a61e3422affcecc3b3778
Xavier Miller <Zebony9@gmail.com>
2026-01-21 13:22:24 +1300

    Complete home page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 import "./style.css";
+import { loadHomePage } from "./home.js"
 
 console.log("Hello, World!");
+
+loadHomePage();
---
+++ src/menu.js
@@ -0,0 +1,8 @@
+export function loadMenuPage() {
+    const content = document.querySelector(".content");
+    content.innerHTML = "";
+
+    const foodItems = document.createElement("section");
+    foodItems.className = "food";
+    
+}
